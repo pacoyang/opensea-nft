74a43d5b1056b534f5b321364562ab6c73afe63d
pacoyang <yangbaigao@gmail.com>
2023-07-10 08:31:26 +0800

    refactor: update data

--- src/app/tokens/[id]/route.ts
+++ src/app/tokens/[id]/route.ts
@@ -4,18 +4,18 @@ const metadata: {[key: string]: any} = {
   '1': {
     'description': 'Friendly OpenSea Creature that enjoys long swims in the ocean.',
     'external_url': 'https://example.com/?token_id=1',
-    'image': 'https://bafybeihslhol5draa26unhhe7j2crwedr4tyfrvmba5qt3kyxbvb5olk4i.ipfs.dweb.link/images/1.png',
-    'name': 'Sprinkles Fisherton'
-  },
-  '2': {
-    'description': 'Friendly OpenSea Creature that enjoys long swims in the ocean.',
-    'external_url': 'https://example.com/?token_id=1',
     'image': 'https://bafybeihslhol5draa26unhhe7j2crwedr4tyfrvmba5qt3kyxbvb5olk4i.ipfs.dweb.link/images/2.png',
     'name': 'Boris McCoy'
   },
+  '2': {
+    'description': 'Friendly OpenSea Creature that enjoys long swims in the ocean.',
+    'external_url': 'https://example.com/?token_id=2',
+    'image': 'https://bafybeihslhol5draa26unhhe7j2crwedr4tyfrvmba5qt3kyxbvb5olk4i.ipfs.dweb.link/images/1.png',
+    'name': 'Sprinkles Fisherton'
+  },
   '3': {
     'description': 'Friendly OpenSea Creature that enjoys long swims in the ocean.',
-    'external_url': 'https://example.com/?token_id=1',
+    'external_url': 'https://example.com/?token_id=3',
     'image': 'https://bafybeihslhol5draa26unhhe7j2crwedr4tyfrvmba5qt3kyxbvb5olk4i.ipfs.dweb.link/images/3.png',
     'name': 'Dave Starbelly'
   }
